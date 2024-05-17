ca9fd1464fd3f35f4ba94d7dc547b166708ab707
J-Andrew4852 <james.andrew4852@gmail.com>
2024-05-17 23:08:49 +0800

    updatedResume

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,7 +1,7 @@
 import { Component } from 'solid-js';
 import styles from './icons.module.css';
 import { JSX } from 'solid-js';
-import cvPdf from '../../assets/Resume-James_Andrew-30_10_2023.pdf';
+import cvPdf from '../../assets/Resume-James_Andrew-17_5_2024.pdf';
 import { Link } from 'solid-app-router';
 
 interface EmailProps {
